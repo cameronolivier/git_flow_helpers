8bfb2a8451fc1cb979b05393ffc5a24760f75579
Cameron Olivier <cameronolivier@gmail.com>
2020-11-11 22:55:08 +0200

    FAAS-456

--- commit.sh
+++ commit.sh
@@ -6,4 +6,4 @@ branch_name=${branch_name:-HEAD}
 jira_id=$(expr "$branch_name" : "[a-z]*/\($project-[0-9][0-9][0-9]\)")
 git commit -a -m "$jira_id $1"
 
-echo "$(pwd)/.xgit_config"
+. "$(pwd)/.xgit_config"
