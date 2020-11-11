ed80b799e3a31defba7b786f0f731bfbb57586ec
Cameron Olivier <cameronolivier@gmail.com>
2020-11-11 22:36:54 +0200

    FAAS-456 some cool commit message

--- commit.sh
+++ commit.sh
@@ -5,5 +5,4 @@ branch_name=${branch_name##refs/heads/}
 branch_name=${branch_name:-HEAD}
 
 task=$(expr "$branch_name" : "[a-z]*/\(FAAS-[0-9][0-9][0-9]\)")
-echo "gxam \"$task $1\""
-#echo "$task"
+git commit -a -m "$task $1"
