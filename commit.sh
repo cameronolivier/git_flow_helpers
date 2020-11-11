c2840813f1b4ac0bf3633faee07ea5e1f0cf181d
Cameron Olivier <cameronolivier@gmail.com>
2020-11-11 22:56:36 +0200

    FAAS-456

--- commit.sh
+++ commit.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-project="FAAS"
+. "$(pwd)/.xgit_config"
+
 branch_name=$(git symbolic-ref -q HEAD)
 branch_name=${branch_name##refs/heads/}
 branch_name=${branch_name:-HEAD}
 jira_id=$(expr "$branch_name" : "[a-z]*/\($project-[0-9][0-9][0-9]\)")
 git commit -a -m "$jira_id $1"
-
-. "$(pwd)/.xgit_config"
-
-echo "$slug"
