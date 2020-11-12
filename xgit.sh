a17b98c94cd058dce881d9d8f31cba31d4678770
Cameron Olivier <cameronolivier@gmail.com>
2020-11-12 12:05:59 +0200

    FAAS-159 refactor(feat): moved branch and commit functionality to functions and calling within if blocks

--- xgit.sh
+++ xgit.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 . "$(pwd)/.xgit_config"
 
-if [ "$1" = "branch" ]
-then
+function branch() {
   name=${3// /_}
   branch_name="$slug$project-$2_$name"
   git checkout -b "$branch_name"
-fi
+}
 
-if [ "$1" = "commit" ]
-then
+function commit() {
   branch_name=$(git symbolic-ref -q HEAD)
   branch_name=${branch_name##refs/heads/}
   branch_name=${branch_name:-HEAD}
   jira_id=$(expr "$branch_name" : "[a-z]*/\($project-[0-9][0-9][0-9]\)")
   git commit -a -m "$jira_id $2"
+}
+
+if [ "$1" = "branch" ]
+then
+  branch "$@"
+fi
+
+if [ "$1" = "commit" ]
+then
+  commit "$@"
 fi
