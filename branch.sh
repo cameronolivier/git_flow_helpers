ab96778c9d7a128a1301a2bf8423ac90d81b179a
Cameron Olivier <cameronolivier@gmail.com>
2020-11-12 12:22:37 +0200

    FAAS-159 removed unused files, updated xgit

--- branch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-. "$(pwd)/.xgit_config"
-
-name=${2// /_}
-branch="$slug$project-$1_$name"
-git checkout -b "$branch"
--- commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. "$(pwd)/.xgit_config"
-
-branch_name=$(git symbolic-ref -q HEAD)
-branch_name=${branch_name##refs/heads/}
-branch_name=${branch_name:-HEAD}
-jira_id=$(expr "$branch_name" : "[a-z]*/\($project-[0-9][0-9][0-9]\)")
-git commit -a -m "$jira_id $1"
